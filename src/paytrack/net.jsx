416fe7c0e304c620cf544b5f91ff566b040291b9
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-03-09 07:21:03 -0500

    debugging local storage - 'deductions' works

--- src/paytrack/net.jsx
+++ src/paytrack/net.jsx
@@ -7,14 +7,17 @@ export default function Net(props) {
     // state for the select deductions bar, checks for stored state in local storage
     // deductionState is the localStorage key - deductionRate is the state
     const [deductionRate, setDeductionRate] = useState(() => {
-        const storedDeductions = localStorage.getItem("deductionState");
+        const storedDeductions = localStorage.getItem('deductionState');
         console.log("stored deductions: ", storedDeductions);
-        return storedDeductions ? JSON.parse(localStorage.getItem(storedDeductions)) : "0.8";
+        return storedDeductions ? console.log("stored value")
+        //JSON.parse(localStorage.getItem(storedDeductions)) 
+        : "0.8";
+
     })
 
     // to update local storage when the select bar options are changed
     useEffect(() => {
-        localStorage.setItem("deductionState", JSON.stringify(deductionRate));
+        localStorage.setItem('deductionState', JSON.stringify(deductionRate));
     }, [deductionRate]);
 
 
@@ -23,16 +26,17 @@ export default function Net(props) {
     useEffect(() => {
       setNetPay(grossPay * deductionRate);
     }, [grossPay, deductionRate]);
-  
+ /* 
     // This sets the default deduction option to 20% on first render
     useEffect(() => {
       const defaultOption = deductionOptions.find(option => option.id === 6);
       setDeductionRate(defaultOption.value);
     }, []);
-    
-  
-    function handleDeductionRate(data){
-      setDeductionRate(data.value);
+    */
+   
+   function handleDeductionRate(data){
+       setDeductionRate(data.value);
+       setDefDeductions(data.value);
     }
   
     const deductionOptions = [
@@ -48,14 +52,28 @@ export default function Net(props) {
       {id: 10, value:0.72, label:"28%"},
       {id: 11, value:0.70, label:"30%"}
     ];
-  
+
+    // trying new version - add local storage deduction state
+        const deductionDefault = localStorage.getItem('deductions');
+        const [defDeductions, setDefDeductions] = useState(
+            () => {
+                return deductionDefault ? deductionDefault : deductionOptions[5];
+            }
+        )
+    
+        useEffect(
+            () => {
+                localStorage.setItem('deductions', defDeductions);
+            }, [deductionRate]
+        );
+    
     return (
       <div>
         <h1>Todays Net Pay: {netPay.toFixed(3)}</h1>
         <label htmlFor="deductions">Deductions Rate</label>
         <div className='dropdown-wrapper'>
           <Select 
-            defaultValue={deductionOptions[5]}
+            defaultValue={deductionDefault}
             title = "change deduction rate"
             className='dropdown-options'
             placeholder={deductionRate ? deductionRate : "Deductions Rate"}
