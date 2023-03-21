5f262ab9e1d72eb9badc039bd52541f239fb86d0
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-03-21 08:08:26 -0400

    responsive hero fix

--- src/portfolio/hero.jsx
+++ src/portfolio/hero.jsx
@@ -5,20 +5,20 @@ export default function hero() {
 
 
   return (
-    <div className='p-8'>
+    <div className='lg:p-8'>
         <div className='flex flex-auto flex-wrap items-center justify-around bg-orange-700'>
             <div className='text-zinc-800 box-content'>
                 <h4 className='text-xl font-bold m-2 tracking-widest'>Front-End Developer</h4>
-                <h1 className='text-5xl font-semibold m-2 mb-12 tracking-wide'>Rich Marmolejo</h1>
-                <div className='flex justify-end max-w-lg'>
+                <h1 className='text-5xl lg:text-6xl font-semibold m-2 mb-12 tracking-wide'>Rich Marmolejo</h1>
+                <div className='flex justify-center max-w-lg md:justify-end'>
                     <a href="mailto:richardmarmolejo@gmail.com" 
-                        className='border py-3 px-10 border-zinc-800 font-semibold tracking-wider box-content hover:bg-orange-800 hover:text-zinc-300'>Contact Me
+                        className='border py-2.5 px-8 lg:px-14 border-zinc-800 font-semibold tracking-wider box-content hover:bg-orange-800 hover:text-zinc-300'>Contact Me
                     </a>
                 </div>
             </div>
             <div>
                 <img src={rich} alt="Rich Marmolejo with his arms folded, and a smile" 
-                    className='max-w-sm md:max-w-xs'
+                    className='max-w-xs lg:max-w-sm'
                 />
             </div>
         </div>
