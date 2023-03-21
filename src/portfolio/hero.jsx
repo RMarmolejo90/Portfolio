260de95ada96c7988573150470fdba03f4667885
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-03-21 07:54:06 -0400

    responsive hero

--- src/portfolio/hero.jsx
+++ src/portfolio/hero.jsx
@@ -6,10 +6,10 @@ export default function hero() {
 
   return (
     <div className='p-8'>
-        <div className='flex flex-auto items-center justify-around bg-orange-700'>
+        <div className='flex flex-auto flex-wrap items-center justify-around bg-orange-700'>
             <div className='text-zinc-800 box-content'>
-                <h4 className='text-lg font-semibold m-2'>Front-End Developer</h4>
-                <h1 className='text-5xl font-semibold m-2 mb-12 '>Rich Marmolejo</h1>
+                <h4 className='text-xl font-bold m-2 tracking-widest'>Front-End Developer</h4>
+                <h1 className='text-5xl font-semibold m-2 mb-12 tracking-wide'>Rich Marmolejo</h1>
                 <div className='flex justify-end max-w-lg'>
                     <a href="mailto:richardmarmolejo@gmail.com" 
                         className='border py-3 px-10 border-zinc-800 font-semibold tracking-wider box-content hover:bg-orange-800 hover:text-zinc-300'>Contact Me
@@ -18,7 +18,7 @@ export default function hero() {
             </div>
             <div>
                 <img src={rich} alt="Rich Marmolejo with his arms folded, and a smile" 
-                    className='max-w-sm'
+                    className='max-w-sm md:max-w-xs'
                 />
             </div>
         </div>
