249dbc7d8270facb83ca806588c35a288d9c6570
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-07-31 20:10:53 -0400

    add the hvac page and router

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,13 @@ module.exports = {
       fontFamily: {
       tilt: "'Tilt Warp', cursive",
       lato: "'Lato', sans-serif"
-      }
+      },
+      backgroundImage:{
+        'gears': "url('./src/assets/Gears.webp')",
+        'secondary-gradient': "linear-gradient(207deg, rgba(21,26,32,1) 30%, rgba(37,40,44,1) 100%);",
+        'primary-gradient': "linear-gradient(90deg, rgba(5,16,76,1) 0%, rgba(24,43,211,1) 50%, rgba(5,16,76,1) 100%);",
+        'heatpump': "url('/assets/HeatPump.webp')", 
+      },
     },
   },
   plugins: [],
