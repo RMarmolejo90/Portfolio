d468cc45622bfd26467ba33a8acc117cc1c8b4cc
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-07-31 20:29:58 -0400

    merges styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,10 +11,10 @@ module.exports = {
       lato: "'Lato', sans-serif"
       },
       backgroundImage:{
-        'gears': "url('./src/assets/Gears.webp')",
+        
         'secondary-gradient': "linear-gradient(207deg, rgba(21,26,32,1) 30%, rgba(37,40,44,1) 100%);",
         'primary-gradient': "linear-gradient(90deg, rgba(5,16,76,1) 0%, rgba(24,43,211,1) 50%, rgba(5,16,76,1) 100%);",
-        'heatpump': "url('/assets/HeatPump.webp')", 
+        
       },
     },
   },
